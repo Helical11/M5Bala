c3bf9d2e2fbbb5c931cfee63113953326b193918
Helical11 <helical@helical-engineer.com>
2023-01-27 09:35:01 +0900

    [add]Hello World

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <Arduino.h>
+#include <M5Stack.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  M5.begin();
+  M5.Lcd.println("Hello World");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
